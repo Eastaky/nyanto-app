3fc3df986beb3233cbcbd95263391b292167eba3
Eastaky <east.taku0124@gmail.com>
2026-08-07 22:11:16 +0900

    背景など装飾とjavascriptを整えた

---
+++ script.js
@@ -0,0 +1,24 @@
+const today = new Date();
+console.log(today);
+
+const year = today.getFullYear();
+console.log(year);
+
+const month = today.getMonth() + 1;
+console.log(month);
+
+const day = today.getDate();
+console.log(day);
+
+const formattedDate = `${year}/${month}/${day}`;
+console.log(formattedDate);
+
+const lastDate = document.getElementById("lastDate");
+
+lastDate.textContent = formattedDate;
+
+const changeButton = document.getElementById("changeButton");
+
+changeButton.addEventListener("click", function () {
+  lastDate.textContent = formattedDate;
+});
